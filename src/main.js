88038197b754b4741b91e998635930038aeffd6c
Milan Munzar <mimunzar@gmail.com>
2021-10-05 22:44:52 +0200

    Navigates to regions

--- src/main.js
+++ src/main.js
@@ -25,32 +25,63 @@ async function mkNewDir(path) {
 }
 
 function normalizeName(s) {
-    return s.normalize("NFD")
-        .replace(/\p{Diacritic}/gu, "")
-        .replaceAll(' ', '_')
+    return s.trim()
+        .normalize("NFD").replace(/\p{Diacritic}/gu, "")
+        .replaceAll(/, | (- )?| /g, '_')
         .toLowerCase();
 }
 
+function takeNth(arr, n) {
+    return arr.reduce((acc, el, idx) => {
+        if (0 < idx % n) return acc;
+        return acc.concat([el]);
+    }, []);
+}
+
 async function downloadRegionClimateStatistics(page, pEl, dst) {
-    console.log(dst);
+    // await pEl.evaluate(el => el.click())
+    // await page.waitForXPath('/html/body/div[2]/div[2]/div/div/div[4]/div/div[2]/div/table[2]');
+    // console.log('##', dst);
     return Promise.resolve();
 }
 
 async function downloadClimateStatistics(page, pEl, dst) {
     await pEl.evaluate(el => el.click())
-    const tableEl = await page.waitForSelector('#loadedcontent table');
-    return Promise.all((await tableEl.$$('a')).map(async (el) => {
-        const region = normalizeName(await el.evaluate((el) => el.innerHTML));
-        return downloadRegionClimateStatistics(page, el, path.join(dst, region));
-    }));
+    const tableEl = await page.waitForXPath(
+        '/html/body/div[2]/div[2]/div/div/div[4]/div/div[2]/div/table');
+
+    let rIdx = 1;
+    for (_ of (await tableEl.$$('tr'))) {
+        for (let cIdx = 1; cIdx < 3; ++cIdx) {
+            const regionLink = (await page.$x(
+                `/html/body/div[2]/div[2]/div/div/div[4]/div/div[2]/div/table/tbody/tr[${rIdx}]/td[${cIdx}]/a`))[0];
+            const regionName = normalizeName(await regionLink.evaluate((el) => el.innerHTML));
+            console.log(' ->', regionName);
+            await downloadRegionClimateStatistics(page, regionLink, path.join(dst, regionName));
+        }
+        rIdx += 1;
+    }
+
+    const backLink = (await page.$x(
+        '/html/body/div[2]/div[2]/div/div/div[4]/div/div[2]/div/a'))[0];
+    await backLink.evaluate((el) => el.click());
+    return page.waitForXPath('/html/body/div[2]/div[2]/div/div/div[4]/div/div[2]/div/ul');
 }
 
-async function downloadStatistics(page, dst) {
-    await mkNewDir(dst);
-    return Promise.all((await page.$$('#loadedcontent li a')).map(async (el) => {
-        const climate = normalizeName(await el.evaluate((el) => el.innerHTML));
-        return downloadClimateStatistics(page, el, path.join(dst, climate));
-    }));
+async function downloadStatistics(browser, dst) {
+    const page = await openMainPage(browser, CHMI_URL);
+
+    let idx = 1;
+    for (_ of (await page.$$('#loadedcontent li a'))) {
+        const climateLink = (await page.$x(
+            `/html/body/div[2]/div[2]/div/div/div[4]/div/div[2]/div/ul/li[${idx}]/a`))[0];
+        const climateName = normalizeName(await climateLink.evaluate((el) => el.innerHTML));
+        console.log('Downloading', climateName);
+        await downloadClimateStatistics(page, climateLink, path.join(dst, climateName));
+        idx += 1;
+    }
+
+    return Promise.resolve();
 }
 
 (async () => {
@@ -58,12 +89,7 @@ async function downloadStatistics(page, dst) {
         headless: false,
         devtools: true,
     });
-
-    const page = await openMainPage(browser, CHMI_URL);
-    await downloadStatistics(page, DATA_DST);
-
-//    await page.waitFor(10000);
-
+    await downloadStatistics(browser, DATA_DST);
     await browser.close();
 })();
 
